e1475450b64dd6998d6d51e94522cba2cc929107
Jean Durand <23022388@sun.ac.za>
2024-05-11 13:32:41 +0100

    Starting with the second experiment

---
+++ scripts/graph4_bash.sh
@@ -0,0 +1,10 @@
+python3 toy_script.py --seeds_replicate 42 --n_observational 100 --n_trials 30 --n_anchor_points 35 --run_num 1
+python3 toy_script.py --seeds_replicate 11 --n_observational 100 --n_trials 30 --n_anchor_points 35 --run_num 2
+python3 toy_script.py --seeds_replicate 89 --n_observational 100 --n_trials 30 --n_anchor_points 35 --run_num 3
+python3 toy_script.py --seeds_replicate 69 --n_observational 100 --n_trials 30 --n_anchor_points 35 --run_num 4
+python3 toy_script.py --seeds_replicate 7 --n_observational 100 --n_trials 30 --n_anchor_points 35 --run_num 5
+python3 toy_script.py --seeds_replicate 27 --n_observational 200 --n_trials 30 --n_anchor_points 35 --run_num 6
+python3 toy_script.py --seeds_replicate 3 --n_observational 200 --n_trials 30 --n_anchor_points 35 --run_num 7
+python3 toy_script.py --seeds_replicate 104 --n_observational 200 --n_trials 30 --n_anchor_points 35 --run_num 8
+python3 toy_script.py --seeds_replicate 1234 --n_observational 200 --n_trials 30 --n_anchor_points 35 --run_num 9
+python3 toy_script.py --seeds_replicate 321 --n_observational 200 --n_trials 30 --n_anchor_points 35 --run_num 10

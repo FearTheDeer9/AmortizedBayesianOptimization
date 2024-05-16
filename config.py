c4bde08b1ed621192e5b68557c7035f84aebf817
Jean Durand <23022388@sun.ac.za>
2024-05-16 21:19:37 +0100

    Update bash scripts

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 SHOW_GRAPHICS = False
-RUN_CEO = True
+RUN_CEO = False
 RUN_CBO = True
 RUN_BO = True
---
+++ scripts/toy_bash_noisy.sh
@@ -0,0 +1,5 @@
+python3 toy_script.py --seeds_replicate 27 --n_observational 200 --n_trials 30 --n_anchor_points 35 --run_num 1 --noiseless False
+python3 toy_script.py --seeds_replicate 3 --n_observational 200 --n_trials 30 --n_anchor_points 35 --run_num 2 --noiseless False
+python3 toy_script.py --seeds_replicate 104 --n_observational 200 --n_trials 30 --n_anchor_points 35 --run_num 3 --noiseless False
+python3 toy_script.py --seeds_replicate 1234 --n_observational 200 --n_trials 30 --n_anchor_points 35 --run_num 4 --noiseless False
+python3 toy_script.py --seeds_replicate 321 --n_observational 200 --n_trials 30 --n_anchor_points 35 --run_num 5 --noiseless False

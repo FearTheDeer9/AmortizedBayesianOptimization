01da0c0d73ffb602e67584fa99ba6baf544217c1
Jean Durand <23022388@sun.ac.za>
2024-05-18 08:37:26 +0100

    Setting a variable correctly

--- scripts/base_script.py
+++ scripts/base_script.py
@@ -60,7 +60,7 @@ def run_script(
 
     # using this as the interventional and observational data
     D_O, D_I, exploration_set = setup_observational_interventional(
-        graph_type="Toy", noiseless=noiseless, seed=seeds_int_data
+        graph_type=graph_type, noiseless=noiseless, seed=seeds_int_data
     )
     # using this as the interventional and observational data
 

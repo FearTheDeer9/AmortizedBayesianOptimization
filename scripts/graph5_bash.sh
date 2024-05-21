ebf3bee235f3d90550bcf2d3ff7746b7c5c38c29
Jean Durand <jd123@texel15.doc.ic.ac.uk>
2024-05-21 09:14:53 +0100

    Adding results for Graph5

--- scripts/graph5_bash.sh
+++ scripts/graph5_bash.sh
@@ -1,5 +1,5 @@
-#python3 graph5_script.py --seeds_replicate 42 --n_observational 200 --n_trials 30 --n_anchor_points 35 --run_num 1 --noiseless
-#python3 graph5_script.py --seeds_replicate 11 --n_observational 200 --n_trials 30 --n_anchor_points 35 --run_num 2 --noiseless
-#python3 graph5_script.py --seeds_replicate 89 --n_observational 200 --n_trials 30 --n_anchor_points 35 --run_num 3 --noiseless
-python3 graph5_script.py --seeds_replicate 69 --n_observational 200 --n_trials 30 --n_anchor_points 35 --run_num 4 --noiseless
-python3 graph5_script.py --seeds_replicate 7 --n_observational 200 --n_trials 30 --n_anchor_points 35 --run_num 5 --noiseless
+python3 graph5_script.py --seeds_replicate 42 --n_observational 200 --n_trials 30 --n_anchor_points 35 --run_num 1 --noiseless
+python3 graph5_script.py --seeds_replicate 11 --n_observational 200 --n_trials 30 --n_anchor_points 35 --run_num 2 --noiseless
+python3 graph5_script.py --seeds_replicate 89 --n_observational 200 --n_trials 30 --n_anchor_points 35 --run_num 3 --noiseless
+#python3 graph5_script.py --seeds_replicate 69 --n_observational 200 --n_trials 30 --n_anchor_points 35 --run_num 4 --noiseless
+#python3 graph5_script.py --seeds_replicate 7 --n_observational 200 --n_trials 30 --n_anchor_points 35 --run_num 5 --noiseless
